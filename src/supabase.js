d2af1c5a9b2a0cb61f25a04f7f0908df6b232aeb
karensflorez-stack <k.sofia.florez@gmail.com>
2026-07-20 13:35:49 -0500

    supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://wipjjksphouirpvydgnu.supabase.co/rest/v1/'
+const supabaseUrl = 'https://wipjjksphouirpvydgnu.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpcGpqa3NwaG91aXJwdnlkZ251Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NTkwODQsImV4cCI6MjEwMDEzNTA4NH0.zqfgPhRq5vz2EVZOmtwYj8AbCI4l8Iwo9AwDsRlpZog'
 
 export const supabase = createClient(
