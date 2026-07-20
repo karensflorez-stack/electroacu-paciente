bfadb86dd95a408912c6e7af782b20873d47c5ae
karensflorez-stack <k.sofia.florez@gmail.com>
2026-07-20 13:14:14 -0500

    Add files via upload

---
+++ src/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://wipjjksphouirpvydgnu.supabase.co/rest/v1/'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpcGpqa3NwaG91aXJwdnlkZ251Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NTkwODQsImV4cCI6MjEwMDEzNTA4NH0.zqfgPhRq5vz2EVZOmtwYj8AbCI4l8Iwo9AwDsRlpZog'
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+)
